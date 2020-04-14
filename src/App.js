859b7097ca6411189030ab32f327c5954e23a231
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-15 05:35:31 +0700

    ADD DASHBOAD

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Map from './pages/MapShow'
 import Chart from './components/Chart'
-
+import Dashboard from './components/Dashboard'
 const App = () => {
 
   return(
     <div>
       {/* <Map /> */}
-      <Chart />
+      <Dashboard />
       </div>
   )
 }
