50fae7085674c521c92d97e1ee61ff3db6c13eea
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-17 01:04:37 +0700

    Add Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import MapShow from './pages/MapShow';
 class App extends Component {
 
   render() {
-
     return (
       <div>
         <BrowserRouter>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -32,26 +32,5 @@ function Sidebar(props) {
     );
 }
 
-
-
 export default Sidebar;
 
-{/* <Navbar bg="light" expand="lg">
-<Navbar.Brand href="#home">
-    <h2>AI ELDERY</h2></Navbar.Brand>
-<Navbar.Toggle aria-controls="basic-navbar-nav" />
-<Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-    </Nav>
-    <Dropdown as={ButtonGroup}>
-        <Button variant="success">
-        <h4>เริ่มต้นใช้งาน</h4></Button>
-        <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
-        <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">ข้อมูลอุบัติผู้สูงอายุ</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">แผนที่การเกิดอุบติเหตุ</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">เกี่ยวกับเรา</Dropdown.Item>
-        </Dropdown.Menu>
-    </Dropdown>
-</Navbar.Collapse>
-</Navbar> */}
