c64c2aba95e39bd9c37ceaa628660576c9d383df
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-19 00:18:08 +0700

    add commit

--- src/pages/MapShow.js
+++ src/pages/MapShow.js
@@ -1,7 +1,6 @@
-
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Icon from '@ant-design/icons';
-import React, { useState, useEffect } from 'react';
 import ReactMapGL, { NavigationControl, Marker, Popup } from 'react-map-gl';
 import Item from 'antd/lib/list/Item';
 import '../App.css'
@@ -9,7 +8,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SidebarMap from '../components/Map/SidebarMap'
 
 //Bootstrap react
-import { Container, Row, Col, ListGroup } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
+
+//MDB
+
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+
+//Bulma
+
+import 'react-bulma-components/basic/react-bulma-components.min.css';
+import { Button, Level, Heading,  } from 'react-bulma-components/dist'
 
 var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
@@ -31,24 +39,50 @@ const MapShow = () => {
             <div>
                 <SidebarMap />
             </div>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-4">
-                        {/* ใส่ข้อมูลรายชื่อผู้ปวดพร้อมที่อยู่ อาการ สถานะ และความเสี่ยงที่เกิด */}
-                    </div>
-                    <div class="col-sm-8" >
-                        <ReactMapGL
-                            {...viewport}
-                            onViewportChange={setViewport}
-                            mapStyle="mapbox://styles/mapbox/outdoors-v11"
-                            mapboxApiAccessToken="pk.eyJ1IjoiaGFtYTg5NyIsImEiOiJjazR6ZG5yY20wOWgzM21tcWVlbnFtOXB4In0.KjTfvistF0bqFqTk0OVsTA">
-                        </ReactMapGL>
-                    </div>
-                </div>
-            </div>
+            <MDBRow>
+                <MDBCol md="4">
+                    <Level mobile renderAs="nav">
+                        <Level.Item textAlignment="centered">
+                            <div>
+                                <p className="heading">tweets</p>
+                                <Heading renderAs="p">3,456</Heading>
+                            </div>
+                        </Level.Item>
+                        <Level.Item textAlignment="centered">
+                            <div>
+                                <p className="heading">following</p>
+                                <Heading renderAs="p">123</Heading>
+                            </div>
+                        </Level.Item>
+                        <Level.Item textAlignment="centered">
+                            <div>
+                                <p className="heading">followers</p>
+                                <Heading renderAs="p">456K</Heading>
+                            </div>
+                        </Level.Item>
+                        <Level.Item textAlignment="centered">
+                            <div>
+                                <p className="heading">likes</p>
+                                <Heading renderAs="p">789</Heading>
+                            </div>
+                        </Level.Item>
+                    </Level>
+
+                </MDBCol>
+                <MDBCol md="8">
+                    <ReactMapGL
+                        {...viewport}
+                        onViewportChange={setViewport}
+                        mapStyle="mapbox://styles/mapbox/outdoors-v11"
+                        mapboxApiAccessToken="pk.eyJ1IjoiaGFtYTg5NyIsImEiOiJjazR6ZG5yY20wOWgzM21tcWVlbnFtOXB4In0.KjTfvistF0bqFqTk0OVsTA">
+                    </ReactMapGL>
+                </MDBCol>
+            </MDBRow>
         </div>
     )
 }
 
 
 export default MapShow;
+
+
