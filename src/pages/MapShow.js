d77ffc01a28b041ba57f881f06fd1867dc977b41
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-18 19:25:05 +0700

    add commit

--- src/pages/MapShow.js
+++ src/pages/MapShow.js
@@ -1,12 +1,15 @@
-// import React, { useState, useEffect, Component } from 'react';
+
 import axios from 'axios'
 import Icon from '@ant-design/icons';
-import React, { useState,useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL, { NavigationControl, Marker, Popup } from 'react-map-gl';
 import Item from 'antd/lib/list/Item';
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SidebarMap from '../components/Map/SidebarMap'
+
+//Bootstrap react
+import { Container, Row, Col } from 'react-bootstrap'
 
 var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
@@ -22,16 +25,30 @@ const MapShow = () => {
 
     });
 
-        return (
+    return (
 
-            <ReactMapGL
-                {...viewport}
-                onViewportChange={setViewport}
-                mapStyle="mapbox://styles/mapbox/outdoors-v11"
-                mapboxApiAccessToken="pk.eyJ1IjoiaGFtYTg5NyIsImEiOiJjazR6ZG5yY20wOWgzM21tcWVlbnFtOXB4In0.KjTfvistF0bqFqTk0OVsTA">
-            </ReactMapGL>
-)
-    }
+        <div>
+            <div>
+                <SidebarMap />
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-4">
+
+                    </div>
+                    <div class="col-sm-8" >
+                        <ReactMapGL
+                            {...viewport}
+                            onViewportChange={setViewport}
+                            mapStyle="mapbox://styles/mapbox/outdoors-v11"
+                            mapboxApiAccessToken="pk.eyJ1IjoiaGFtYTg5NyIsImEiOiJjazR6ZG5yY20wOWgzM21tcWVlbnFtOXB4In0.KjTfvistF0bqFqTk0OVsTA">
+                        </ReactMapGL>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 
 export default MapShow;
