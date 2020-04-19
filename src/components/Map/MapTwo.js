4964af09dea02fe1afcad47343736ca56b4b0dac
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-19 18:42:33 +0700

    Add commit

--- src/components/Map/MapTwo.js
+++ src/components/Map/MapTwo.js
@@ -3,20 +3,21 @@ import L from 'leaflet'
 import './Map.css'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
-// type State = {
-//     lat: number,
-//     lng: number,
-//     zoom: number,
-//   }
-  
+var myIcon = L.icon({
 
+    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/512px-Map_marker.svg.png',
+    iconSize: [30, 51],
+    iconAnchor: [12.5, 41],
+    popupAnchor: [0, -41]
+
+});
 
 class MapTwo extends Component {
 
 
     state = {
-        lat: 51.505,
-        lng: -0.09,
+        lat: 7.8948795,
+        lng: 98.350931,
         zoom: 13,
       }
 
@@ -26,13 +27,13 @@ class MapTwo extends Component {
    const position = [this.state.lat, this.state.lng]
   
         return (
-            <div className="map">
-            <Map center={position} zoom={this.state.zoom}>
+            <div>
+            <Map className="map" center={position} zoom={this.state.zoom}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker position={position} icon={myIcon}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
               </Popup>
