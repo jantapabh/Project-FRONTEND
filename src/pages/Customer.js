009d2f968ee4ee37cf896715583b66bbf3647191
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-19 22:07:09 +0700

    add commit

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MapTwo from '../components/Map/MapTwo'
 
 //ส่วนสำหรับเก็บข้อมูลของ users ที่ใช้งานระบบ
+// เก็บข้อฒูล user ในรูปแบบ Card ซึ่งสามารถกดเพื่อดูข้อฒูแต่ในคนได้ 
 
 const Customer = () => {
 
