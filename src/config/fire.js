39ecf9e0f0c2d828804b78632d0eecf630f935e7
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-22 09:37:59 +0700

    ADD commit test

--- src/config/fire.js
+++ src/config/fire.js
@@ -2,17 +2,17 @@ import firebase from 'firebase'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCbtxpjI4-4yvfGl04rsRIujMrOs7wVsvg",
-    authDomain: "prepar-project.firebaseapp.com",
-    databaseURL: "https://prepar-project.firebaseio.com",
-    projectId: "prepar-project",
-    storageBucket: "prepar-project.appspot.com",
-    messagingSenderId: "109478906982",
-    appId: "1:109478906982:web:9263fd45d8870f3411b1a7",
-    measurementId: "G-0H2LCE6H2T"
-  };
+  apiKey: "AIzaSyCbtxpjI4-4yvfGl04rsRIujMrOs7wVsvg",
+  authDomain: "prepar-project.firebaseapp.com",
+  databaseURL: "https://prepar-project.firebaseio.com",
+  projectId: "prepar-project",
+  storageBucket: "prepar-project.appspot.com",
+  messagingSenderId: "109478906982",
+  appId: "1:109478906982:web:9263fd45d8870f3411b1a7",
+  measurementId: "G-0H2LCE6H2T"
+};
 
-  const fire = firebase.initializeApp(firebaseConfig)
+const fire = firebase.initializeApp(firebaseConfig)
 
-  export default fire;
+export default fire;
 
