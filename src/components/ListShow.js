30995795b981157197ef9ae2355b5a80f98b14c4
jantapabh <jantapa2407@gmail.com>
2020-04-24 13:52:04 +0700

    Add commit test

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-
+import { ListGroup } from 'react-bootstrap'
 
 //แสดงรายการในส่วน map สำหรับส่วนข้อมูลที่จำเป็น สถานะ การแจ้งเตือน โอกาสเสี่ยง
 
@@ -9,9 +8,24 @@ const ListShow = () => {
 
   return (
     <div>
-
-      Hello World
-
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            Click me!
+    </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Hello! I'm the body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="1">
+            Click me!
+    </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     </div>
   )
 }
