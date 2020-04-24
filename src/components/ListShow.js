e20cdba8439fbb781e0555cbae7fbbaacd82c7fa
jantapabh <jantapa2407@gmail.com>
2020-04-24 17:09:22 +0700

    add commit test

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -22,11 +22,6 @@ const ListShow = () => {
         </Modal.Header>
         <Modal.Body>
           <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>
