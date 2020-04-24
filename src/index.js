cbaf5a150885daa2e44dec4f6c9f291f4c063106
jantapabh <jantapa2407@gmail.com>
2020-04-24 12:56:20 +0700

    Add commit reden pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM, {render} from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter, Router, Route, Link} from 'react-router-dom'
-import {browserHistory} from 'react-router'
+import { BrowserRouter, Router, Route, Link } from 'react-router-dom'
+import { browserHistory } from 'react-router'
 
 //Leaflet
 
