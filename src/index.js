00a715f7a225b309cf5e519ea0798529d5df335e
jantapabh <jantapa2407@gmail.com>
2020-04-24 12:56:47 +0700

    Add map

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Router, Route, Link } from 'react-router-dom'
-import { browserHistory } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
 //Leaflet
 
