360dee21207beb303f02c7a9ebb28b6bcc3e42c1
jantapabh <jantapa2407@gmail.com>
2020-04-25 18:08:34 +0700

    Add change

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -60,7 +60,7 @@ const ListShow = props => {
           </Accordion.Collapse>
         </Card>
       </Accordion> */}
-      
+
       <Toast>
         <Toast.Header>
           <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
@@ -73,7 +73,7 @@ const ListShow = props => {
         <Toast.Header>
           <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
           <strong className="mr-auto">Bootstrap</strong>
-          <small>2 seconds ago</small>
+          <small>2 second ago</small>
         </Toast.Header>
         <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
       </Toast>
