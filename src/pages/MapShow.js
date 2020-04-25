0360c23ba145b0b7b503ce3369d1f8b7dae0b2bc
jantapabh <jantapa2407@gmail.com>
2020-04-25 22:36:28 +0700

    Add commit

--- src/pages/MapShow.js
+++ src/pages/MapShow.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SidebarMap from '../components/Map/SidebarMap'
-import ListShow from '../components/ListShow'
 
 //Bootstrap react
 import { Card } from 'react-bootstrap'
@@ -14,7 +12,11 @@ import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 //Bulma
 
 import { Button, Level, Heading, } from 'react-bulma-components/dist'
+
+//Components Based
 import MapTwo from '../components/Map/MapTwo';
+import SidebarMap from '../components/Map/SidebarMap'
+import ListShow from '../components/ListShow'
 
 
 const MapShow = () => {
