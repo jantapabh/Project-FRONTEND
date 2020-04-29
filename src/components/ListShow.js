3b8a0e4835dbef054eb5ea9ab2da3e034ad9586d
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-29 23:25:41 +0700

    Add commit

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -32,7 +32,9 @@ const ListShow = props => {
       <Toast>
         <Toast.Body>
           <Row>
-            <Col xs="6">.col-6</Col>
+            <Col xs="6">
+              <Button outline color="primary">primary</Button>{' '}
+              </Col>
             <Col xs="6">
               <Button color="danger" onClick={toggle}>ดูข้อมูล</Button>
               <Modal isOpen={modal} toggle={toggle} className={className}>
@@ -60,7 +62,69 @@ const ListShow = props => {
       <Toast>
         <Toast.Body>
           <Row>
-            <Col xs="6">.col-6</Col>
+            <Col xs="6">
+              <Button outline color="primary">primary</Button>{' '}
+            </Col>
+            <Col xs="6">
+              <Button color="danger" onClick={toggle}>ดูข้อมูล</Button>
+              <Modal isOpen={modal} toggle={toggle} className={className}>
+                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <br />
+                  <Modal isOpen={nestedModal} toggle={toggleNested} onClosed={closeAll ? toggle : undefined}>
+                    <ModalHeader>Nested Modal title</ModalHeader>
+                    <ModalBody>Stuff and things</ModalBody>
+                    <ModalFooter>
+                      <Button color="primary" onClick={toggleNested}>Done</Button>{' '}
+                      <Button color="secondary" onClick={toggleAll}>All Done</Button>
+                    </ModalFooter>
+                  </Modal>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="secondary" onClick={toggle}>Cancel</Button>
+                </ModalFooter>
+              </Modal>
+            </Col>
+          </Row>
+        </Toast.Body>
+      </Toast>
+      <Toast>
+        <Toast.Body>
+          <Row>
+            <Col xs="6">
+              <Button outline color="primary">primary</Button>{' '}
+            </Col>
+            <Col xs="6">
+              <Button color="danger" onClick={toggle}>ดูข้อมูล</Button>
+              <Modal isOpen={modal} toggle={toggle} className={className}>
+                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <br />
+                  <Modal isOpen={nestedModal} toggle={toggleNested} onClosed={closeAll ? toggle : undefined}>
+                    <ModalHeader>Nested Modal title</ModalHeader>
+                    <ModalBody>Stuff and things</ModalBody>
+                    <ModalFooter>
+                      <Button color="primary" onClick={toggleNested}>Done</Button>{' '}
+                      <Button color="secondary" onClick={toggleAll}>All Done</Button>
+                    </ModalFooter>
+                  </Modal>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="secondary" onClick={toggle}>Cancel</Button>
+                </ModalFooter>
+              </Modal>
+            </Col>
+          </Row>
+        </Toast.Body>
+      </Toast>
+      <Toast>
+        <Toast.Body>
+          <Row>
+            <Col xs="6">
+              <Button outline color="primary">primary</Button>{' '}
+            </Col>
             <Col xs="6">
               <Button color="danger" onClick={toggle}>ดูข้อมูล</Button>
               <Modal isOpen={modal} toggle={toggle} className={className}>
