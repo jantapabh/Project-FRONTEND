1753c3b514182afcfcac683f1e04d3db5b31a188
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-29 23:01:05 +0700

    add commit

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -12,7 +12,6 @@ const ListShow = props => {
       <Toast>
         <Toast.Header>
           <strong className="mr-auto">Bootstrap</strong>
-          <small>just now</small>
         </Toast.Header>
         <Toast.Body>See? Just like this.</Toast.Body>
       </Toast>
@@ -20,7 +19,6 @@ const ListShow = props => {
         <Toast.Header>
           <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
           <strong className="mr-auto">Bootstrap</strong>
-          <small>2 second ago</small>
         </Toast.Header>
         <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
       </Toast>
