1e25d6c9c88cc3164067f6edceb6c1e1ae709053
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-29 22:03:44 +0700

    Add commit change

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -5,60 +5,10 @@ import { Accordion, Card, Modal, Button, Toast } from 'react-bootstrap'
 
 
 const ListShow = props => {
-  
-  const MyVerticallyCenteredModal = props => {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>Centered Modal</h4>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-
-  const [modalShow, setModalShow] = React.useState(false);
 
   return (
+
     <div style={{ margin: 5, padding: 5}}>
-      
-      {/* <Accordion defaultActiveKey="0">
-        <Card>
-            <Button variant="primary" onClick={() => setModalShow(true)}>
-              Launch vertically centered modal
-      </Button>
-
-            <MyVerticallyCenteredModal
-              show={modalShow}
-              onHide={() => setModalShow(false)}
-            />
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>Hello! I'm the body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Accordion.Toggle as={Card.Header} eventKey="1">
-            Click me!
-    </Accordion.Toggle>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm another body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion> */}
-
       <Toast>
         <Toast.Header>
           <strong className="mr-auto">Bootstrap</strong>
@@ -75,6 +25,7 @@ const ListShow = props => {
         <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
       </Toast>
     </div>
+    
   )
 }
 export default ListShow;
