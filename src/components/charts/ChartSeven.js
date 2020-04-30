f9218c1d17399a2c8036a56bcb5eaf8e7e837a64
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-01 06:05:22 +0700

    add chart

--- src/components/charts/ChartSeven.js
+++ src/components/charts/ChartSeven.js
@@ -1,50 +1,90 @@
 import React, { Component} from 'react'
 import ReactApexChart from 'react-apexcharts'
 
-
 class ChartSeven extends Component {
     constructor(props) {
-        super(props);
+      super(props);
 
-        this.state = {
-
-            series: [44, 55, 13, 43, 22],
-            options: {
-                chart: {
-                    width: 380,
-                    type: 'pie',
-                },
-                labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
-                responsive: [{
-                    breakpoint: 480,
-                    options: {
-                        chart: {
-                            width: 200
-                        },
-                        legend: {
-                            position: 'bottom'
-                        }
-                    }
-                }]
+      this.state = {
+      
+        series: [{
+          name: 'XYZ MOTORS',
+          data: dates
+        }],
+        options: {
+          chart: {
+            type: 'area',
+            stacked: false,
+            height: 350,
+            zoom: {
+              type: 'x',
+              enabled: true,
+              autoScaleYaxis: true
             },
-
-
-        };
+            toolbar: {
+              autoSelected: 'zoom'
+            }
+          },
+          dataLabels: {
+            enabled: false
+          },
+          markers: {
+            size: 0,
+          },
+          title: {
+            text: 'Stock Price Movement',
+            align: 'left'
+          },
+          fill: {
+            type: 'gradient',
+            gradient: {
+              shadeIntensity: 1,
+              inverseColors: false,
+              opacityFrom: 0.5,
+              opacityTo: 0,
+              stops: [0, 90, 100]
+            },
+          },
+          yaxis: {
+            labels: {
+              formatter: function (val) {
+                return (val / 1000000).toFixed(0);
+              },
+            },
+            title: {
+              text: 'Price'
+            },
+          },
+          xaxis: {
+            type: 'datetime',
+          },
+          tooltip: {
+            shared: false,
+            y: {
+              formatter: function (val) {
+                return (val / 1000000).toFixed(0)
+              }
+            }
+          }
+        },
+      
+      
+      };
     }
 
-
+  
 
     render() {
-        return (
+      return (
+        
+
+  <div id="chart">
+<ReactApexChart options={this.state.options} series={this.state.series} type="area" height={350} />
+</div>
 
 
-            <div id="chart">
-                <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width={280} />
-            </div>
-
-
-        );
+      );
     }
-}
+  }
 
 export default ChartSeven
