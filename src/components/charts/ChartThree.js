4f57ff51d328aa39472f63e0d2000dc64f945ec8
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-30 23:53:36 +0700

    Add chart finish

--- src/components/charts/ChartThree.js
+++ src/components/charts/ChartThree.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react'
 import ReactApexChart from 'react-apexcharts'
 
 
-class ApexChart extends Component {
+class ChartThree extends Component {
     constructor(props) {
         super(props);
 
