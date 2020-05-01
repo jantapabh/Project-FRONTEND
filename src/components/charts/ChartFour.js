43cfc41b858201a49f9782f3720d88fcb51e214e
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-01 23:38:31 +0700

    add commit chart

--- src/components/charts/ChartFour.js
+++ src/components/charts/ChartFour.js
@@ -3,77 +3,81 @@
 import React, { Component } from 'react'
 import ReactApexChart from 'react-apexcharts'
 
+
+
 class ChartFour extends Component {
-
     constructor(props) {
-        super(props);
-        this.state = {
-            series: [{
-                name: 'Likes',
-                data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5]
-            }],
-            options: {
-                chart: {
-                    height: 350,
-                    type: 'line',
-                },
-                stroke: {
-                    width: 7,
-                    curve: 'smooth'
-                },
-                xaxis: {
-                    type: 'datetime',
-                    categories: ['1/11/2000', '2/11/2000', '3/11/2000', '4/11/2000', '5/11/2000', '6/11/2000', '7/11/2000', '8/11/2000', '9/11/2000', '10/11/2000', '11/11/2000', '12/11/2000', '1/11/2001', '2/11/2001', '3/11/2001', '4/11/2001', '5/11/2001', '6/11/2001'],
-                },
-                title: {
-                    align: 'left',
-                    style: {
-                        fontSize: "16px",
-                        color: '#666'
-                    }
-                },
-                fill: {
-                    type: 'gradient',
-                    gradient: {
-                        shade: 'dark',
-                        gradientToColors: ['#FDD835'],
-                        shadeIntensity: 1,
-                        type: 'horizontal',
-                        opacityFrom: 1,
-                        opacityTo: 1,
-                        stops: [0, 100, 100, 100]
-                    },
-                },
-                markers: {
-                    size: 4,
-                    colors: ["#FFA41B"],
-                    strokeColors: "#fff",
-                    strokeWidth: 2,
-                    hover: {
-                        size: 7,
-                    }
-                },
-                yaxis: {
-                    min: -10,
-                    max: 40,
-                    title: {
-                        text: 'Engagement',
-                    },
-                }
+      super(props);
+
+      this.state = {
+      
+        series: [{
+          name: '80 ปีขึ้นไป',
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+        }, {
+          name: '70-79 ปี',
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        }, {
+          name: '60-69 ปี',
+          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+        }],
+        options: {
+          chart: {
+            type: 'bar',
+            height: 350
+          },
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '55%',
+              endingShape: 'rounded'
             },
-
-
-        };
+          },
+          dataLabels: {
+            enabled: false
+          },
+          stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+          },
+          xaxis: {
+            categories: ['2551', '2552', '2553', '2554', '2555', '2556', '2557', '2558'],
+          },
+          yaxis: {
+            title: {
+              text: '$ (thousands)'
+            }
+          },
+          fill: {
+            opacity: 1
+          },
+          tooltip: {
+            y: {
+              formatter: function (val) {
+                return "$ " + val + " thousands"
+              }
+            }
+          }
+        },
+      
+      
+      };
     }
+
+  
 
     render() {
-        return (
-            <div id="chart">
-                <ReactApexChart options={this.state.options} series={this.state.series} type="line" height={210} />
-            </div>
-        );
-    }
-}
+      return (
+        
 
+  <div id="chart">
+<ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={210} />
+</div>
+
+
+      );
+    }
+  }
 
 export default ChartFour;
