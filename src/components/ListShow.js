5523225185b64b97d3ab240ee137920b6ce59eb3
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-02 01:15:40 +0700

    add commit

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Toast } from 'react-bootstrap'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Col, Row } from 'reactstrap';
+
 
 //แสดงรายการในส่วน map สำหรับส่วนข้อมูลที่จำเป็น สถานะ การแจ้งเตือน โอกาสเสี่ยง
+
 
 
 const ListShow = props => {
@@ -12,20 +14,11 @@ const ListShow = props => {
     className
   } = props;
 
-
   const [modal, setModal] = useState(false);
-  const [nestedModal, setNestedModal] = useState(false);
-  const [closeAll, setCloseAll] = useState(false);
 
   const toggle = () => setModal(!modal);
-  const toggleNested = () => {
-    setNestedModal(!nestedModal);
-    setCloseAll(false);
-  }
-  const toggleAll = () => {
-    setNestedModal(!nestedModal);
-    setCloseAll(true);
-  }
+
+
 
   return (
 
@@ -35,17 +28,16 @@ const ListShow = props => {
           <Row>
             <Col xs="7">
               <Button outline color="primary">ผู้สูงอายุคนที่ 1</Button>{' '}
-              </Col>
+            </Col>
             <Col xs="5">
               <Button color="danger" onClick={toggle}>ดูข้อมูล</Button>
-              <Modal isOpen={modal} toggle={toggle} className={className}>
+              <Modal isOpen={modal} fade={false} toggle={toggle} className={className}>
                 <ModalHeader toggle={toggle}>ข้อมูลผู้ใช้</ModalHeader>
                 <ModalBody>
-                  
-                  {/* เพิ่มข้อมูลผู้ใข้ */}
-         
-                </ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
                 <ModalFooter>
+                  <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
                   <Button color="secondary" onClick={toggle}>Cancel</Button>
                 </ModalFooter>
               </Modal>
@@ -61,16 +53,17 @@ const ListShow = props => {
             </Col>
             <Col xs="5">
               <Button color="danger" onClick={toggle}>ดูข้อมูล</Button>
-              <Modal isOpen={modal} toggle={toggle} className={className}>
+              <Modal isOpen={modal} fade={false} toggle={toggle} className={className}>
                 <ModalHeader toggle={toggle}>ข้อมูลผู้ใช้</ModalHeader>
                 <ModalBody>
-                  {/* เพิ่มข้อมูลผู้ใข้ */}
-          <br />
-                </ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
                 <ModalFooter>
+                  <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
                   <Button color="secondary" onClick={toggle}>Cancel</Button>
                 </ModalFooter>
               </Modal>
+
             </Col>
           </Row>
         </Toast.Body>
@@ -83,15 +76,17 @@ const ListShow = props => {
             </Col>
             <Col xs="5">
               <Button color="danger" onClick={toggle}>ดูข้อมูล</Button>
-              <Modal isOpen={modal} toggle={toggle} className={className}>
-                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+              <Modal isOpen={modal} fade={false} toggle={toggle} className={className}>
+                <ModalHeader toggle={toggle}>ข้อมูลผู้ใช้</ModalHeader>
                 <ModalBody>
-                 {/* เพิ่มข้อมูลผู้ใข้ */}
-                 </ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
                 <ModalFooter>
+                  <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
                   <Button color="secondary" onClick={toggle}>Cancel</Button>
                 </ModalFooter>
               </Modal>
+
             </Col>
           </Row>
         </Toast.Body>
@@ -104,18 +99,17 @@ const ListShow = props => {
             </Col>
             <Col xs="5">
               <Button color="danger" onClick={toggle}>ดูข้อมูล</Button>
-              <Modal isOpen={modal} toggle={toggle} className={className}>
-                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+              <Modal isOpen={modal} fade={false} toggle={toggle} className={className}>
+                <ModalHeader toggle={toggle}>ข้อมูลผู้ใช้</ModalHeader>
                 <ModalBody>
-                 
-                  {/* เพิ่มข้อมูลผู้ใข้ */}
-          <br />
-
-                </ModalBody>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
                 <ModalFooter>
+                  <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
                   <Button color="secondary" onClick={toggle}>Cancel</Button>
                 </ModalFooter>
               </Modal>
+
             </Col>
           </Row>
         </Toast.Body>
