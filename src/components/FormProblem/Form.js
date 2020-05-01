d3e53cbb37b78156290977cb839a23f3d95ca334
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-02 03:37:23 +0700

    add commit

--- src/components/FormProblem/Form.js
+++ src/components/FormProblem/Form.js
@@ -1,12 +1,12 @@
 import React, { Component, useState } from 'react'
-import StickyFooter from './StickFooter'
+
 
 
 const Form = () => {
 
     return (
         <div>
-            <StickyFooter />
+         
         </div>
     )
 }
