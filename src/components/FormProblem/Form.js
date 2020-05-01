fbfa9717e75758501d0a0eb9cc23f788de35d039
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-02 05:26:00 +0700

    add commit

--- src/components/FormProblem/Form.js
+++ src/components/FormProblem/Form.js
@@ -9,10 +9,10 @@ const Form = () => {
         <MDBContainer>
             <MDBRow>
                 <MDBCol md="50">
-                    <form>
+                    <form style={{ borderBlockStyle: 'dotted'}}>
                         <p className="h3 text-center mb-4" style={{ backgroundColor: 'red', margin: 20, padding: 20, borderRadius: 50}}>Write Problem on Your Device</p>
                         <label htmlFor="defaultFormContactNameEx" className="grey-text">
-                            Your name
+                            Your Company
         </label>
                         <input type="text" id="defaultFormContactNameEx" className="form-control" />
                         <br />
@@ -22,12 +22,12 @@ const Form = () => {
                         <input type="email" id="defaultFormContactEmailEx" className="form-control" />
                         <br />
                         <label htmlFor="defaultFormContactSubjectEx" className="grey-text">
-                            Subject
+                            Your Telephone
         </label>
                         <input type="text" id="defaultFormContactSubjectEx" className="form-control" />
                         <br />
                         <label htmlFor="defaultFormContactMessageEx" className="grey-text">
-                            Your message
+                            Your Problem
         </label>
                         <textarea type="text" id="defaultFormContactMessageEx" className="form-control" rows="3" />
                         <div className="text-center mt-4">
