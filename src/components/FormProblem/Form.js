1ae812c767efeab6fe7fe8be0e6bb0cc7e3db16b
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-02 03:33:13 +0700

    add commit test footer

---
+++ src/components/FormProblem/Form.js
@@ -0,0 +1,14 @@
+import React, { Component, useState } from 'react'
+import StickyFooter from './StickFooter'
+
+
+const Form = () => {
+
+    return (
+        <div>
+            <StickyFooter />
+        </div>
+    )
+}
+
+export default Form
