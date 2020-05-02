337f9bcd577333c49a3dfcc5a43e88a872ea104b
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-02 22:31:49 +0700

    add commit

--- src/components/FormProblem/Form.js
+++ src/components/FormProblem/Form.js
@@ -1,47 +1,131 @@
-import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Form = () => {
-    
-    return (
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-        <div style={{ margin: 10, backgroundColor: 'gray', padding: 20, borderRadius: 100}}>
-        <MDBContainer>
-            <MDBRow>
-                <MDBCol md="50">
-                    <form style={{ borderBlockStyle: 'dotted'}}>
-                        <p className="h3 text-center mb-4" style={{ backgroundColor: 'red', margin: 20, padding: 20, borderRadius: 50}}>Write Problem on Your Device</p>
-                        <label htmlFor="defaultFormContactNameEx" className="grey-text">
-                            Your Company
-        </label>
-                        <input type="text" id="defaultFormContactNameEx" className="form-control" />
-                        <br />
-                        <label htmlFor="defaultFormContactEmailEx" className="grey-text">
-                            Your email
-        </label>
-                        <input type="email" id="defaultFormContactEmailEx" className="form-control" />
-                        <br />
-                        <label htmlFor="defaultFormContactSubjectEx" className="grey-text">
-                            Your Telephone
-        </label>
-                        <input type="text" id="defaultFormContactSubjectEx" className="form-control" />
-                        <br />
-                        <label htmlFor="defaultFormContactMessageEx" className="grey-text">
-                            Your Problem
-        </label>
-                        <textarea type="text" id="defaultFormContactMessageEx" className="form-control" rows="3" />
-                        <div className="text-center mt-4">
-                            <MDBBtn color="warning"  type="submit" style={{ width: 150, height: 50}}>
-                                Send
-                    <MDBIcon far icon="paper-plane" className="ml-2" />
-                            </MDBBtn>
-                        </div>
-                    </form>
-                </MDBCol>
-            </MDBRow>
-        </MDBContainer>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Form() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
         </div>
-    );
-};
-
-export default Form;
+      </Grid>
+    </Grid>
+  );
+}
