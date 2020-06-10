7c1e61b1003f9ae6c402327b81483b77797896ce
jantapabh <s6035512034@phuket.psu.ac.th>
2020-06-10 23:20:16 +0700

    add commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -132,7 +132,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Dashboard() {
-
     const classes = useStyles();
     const [open, setOpen] = React.useState(true);
     const handleDrawerOpen = () => {
