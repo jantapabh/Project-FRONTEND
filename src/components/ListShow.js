c4f37f2f843d81d9ea3d0556fce84e7157f3664e
jantapabh <s6035512034@phuket.psu.ac.th>
2020-06-12 18:12:35 +0700

    add description

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -3,7 +3,6 @@ import { Toast, Modal, Button, Row, Col } from 'react-bootstrap'
 import { ListGroup, ListGroupItem, Progress } from 'reactstrap';
 
 //แสดงรายการในส่วน map สำหรับส่วนข้อมูลที่จำเป็น สถานะ การแจ้งเตือน โอกาสเสี่ยง
-
 const ListShow = props => {
   function MyVerticallyCenteredModal(props) {
     return (
