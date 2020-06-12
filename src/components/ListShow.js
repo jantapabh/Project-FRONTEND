d0062d2d31d8880d694a8baa11e2fbd6ffa5b9b5
jantapabh <s6035512034@phuket.psu.ac.th>
2020-06-12 18:14:11 +0700

    add description

--- src/components/ListShow.js
+++ src/components/ListShow.js
@@ -12,6 +12,7 @@ const ListShow = props => {
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
+        
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
             ข้อมูลผู้ใช้
