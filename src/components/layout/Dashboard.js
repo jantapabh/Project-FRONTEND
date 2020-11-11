e705d62f91118ac2368915ccb1e2b7ac2d90385d
jantapabh <s6035512034@phuket.psu.ac.th>
2020-11-12 04:18:07 +0700

    add commit

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -3,15 +3,18 @@ import React, { useState, useEffect } from 'react';
 import Login from '../../pages/Login';
 
 
-const Dashboard = props => {
 
 
+
+const Dashboard = () => {
+
+ 
+  
     return (
-        < React.Fragment>
-
-        </ React.Fragment>
+    <div>
+      Dashboard
+      </div>
     );
-}
-
-
-export default Dashboard
+  };
+  
+  export default Dashboard;
