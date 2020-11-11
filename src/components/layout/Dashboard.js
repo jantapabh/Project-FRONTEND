e39d71c692dac70b2a8bd9af47656cf8f2e62123
jantapabh <s6035512034@phuket.psu.ac.th>
2020-11-12 04:21:22 +0700

    add commit

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -3,13 +3,9 @@ import React, { useState, useEffect } from 'react';
 import Login from '../../pages/Login';
 
 
-
-
-
+//หน้าหลลักแสดงในส่วนของหลัก Login
 const Dashboard = () => {
-
  
-  
     return (
     <div>
       Dashboard
