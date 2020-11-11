487c8ae28187740468521dbdf7b01dc7410b0820
jantapabh <s6035512034@phuket.psu.ac.th>
2020-11-11 20:18:37 +0700

    add commit

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,10 +1,8 @@
 
 import React, { useState, useEffect } from 'react';
 
-
-import NavBar from './NavBar';
 import Error from '../error'
-import Map from './Map'
+
 
 
 const Dashboard = props => {
@@ -12,7 +10,7 @@ const Dashboard = props => {
 
     return (
         < React.Fragment>
-       <Map />
+  
         </ React.Fragment>
     );
 }
