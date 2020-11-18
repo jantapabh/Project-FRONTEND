b565215c95390ec64e32c2d4f7306eff7a5aa345
jantapabh <s6035512034@phuket.psu.ac.th>
2020-11-19 02:58:21 +0700

    add commit

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -25,7 +25,7 @@ const NavigationBar = () => {
         <div>
             <Styles>
                 <Navbar expand="lg">
-                    <Navbar.Brand href="/">Tutorial</Navbar.Brand>
+                    <Navbar.Brand href="/">AI ELDERY</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Form className="form-center">
                         <FormControl type="text" placeholder="Search" className="" />
@@ -33,7 +33,7 @@ const NavigationBar = () => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
                             <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                            <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link href="/">ออกจากระบบ</Nav.Link></Nav.Item>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
