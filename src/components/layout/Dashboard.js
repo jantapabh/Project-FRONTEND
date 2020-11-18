a91e9f7d0019ca98d57f82889d869b0f3108518b
jantapabh <s6035512034@phuket.psu.ac.th>
2020-11-19 02:56:20 +0700

    add commit

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,15 +1,38 @@
 
 import React, { useState, useEffect } from 'react';
-import { Button, DatePicker } from 'antd';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { Row, Col } from 'antd';
+import LoginBar from '../LoginBar'
+import styled from 'styled-components';
+import NavigationBar from '../NavigationBar'
+import NavBar from '../NavBar'
+
+const GridWrapper = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  margin-top: 1em;
+  margin-left: 6em;
+  margin-right: 6em;
+  grid-template-columns: repeat(12, 1fr);
+  grid-auto-rows: minmax(25px, auto);
+`;
+
+
+//แสดงส่วนการเรียกใช้งาน NavBar
 
 //หน้าหลลักแสดงในส่วนของหลัก Login
 const Dashboard = () => {
 
   return (
     <div>
-
+      <div>
+        <NavigationBar />
+        <NavBar />
+      </div>
     </div>
-  );
-};
+  )
+}
 
 export default Dashboard;
+
