4c8529d797ae534398c60622f93bd2e78df14123
jantapabh <s6035512034@phuket.psu.ac.th>
2020-11-19 01:51:29 +0700

    add commit

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,16 +1,25 @@
 
 import React, { useState, useEffect } from 'react';
 import Login from '../../pages/Login';
-
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+import GlobalStyles from '../GlobalStyles';
+import 'src/mixins/chartjs';
+import theme from 'src/theme';
+import routes from 'src/routes';
 
 //หน้าหลลักแสดงในส่วนของหลัก Login
 const Dashboard = () => {
- 
-    return (
+
+  return (
     <div>
-     fdfd
-      </div>
-    );
-  };
-  
-  export default Dashboard;
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        {routing}
+      </ThemeProvider>
+    </div>
+  );
+};
+
+export default Dashboard;
